36789b811202897c59ef16b7e17c39172177b95e
liangjz <liangjz15@mails.tsinghua.edu.cn>
2018-05-30 19:28:10 +0800

    在TableCreate.java中添加了断点插入的功能

--- src/dbperf/TableCreate.java
+++ src/dbperf/TableCreate.java
@@ -9,12 +9,14 @@ import java.io.*;
 public class TableCreate {
     //文件名为参数的第一个
     //第2,3,4分别为连接的url, 用户名和密码
+    //第5个可选参数表示从这一行起开始进行插入
     public static void main(String[] args) {
         if (args.length < 4){
             System.out.println(String.format("only %d arguments given", args.length));
             System.out.println("filename, url, username, pass needed");
             return;
         }
+        int startLine = args.length > 4 ? Integer.parseInt(args[4]) : 0;
         Connection con = null;
         Statement stmt = null;
         String line = null;
@@ -29,7 +31,12 @@ public class TableCreate {
             //逐条执行文件中的语句
             stmt = con.createStatement();
             int statementCount = 0;
+            int lineCount = 0;
             while((line = bufferReader.readLine()) != null){
+                if(lineCount < startLine){
+                    ++lineCount;
+                    continue;
+                }
                 stmt.executeUpdate(line);
                 ++statementCount;
                 if(statementCount % 10000 == 0){
